2742de05618a1cfb0ac1f21d5fb13ddb4cb4669d
RyanHubOG <ryanhulscher5@gmail.com>
2025-09-14 02:26:45 +0200

    Update loader.lua

--- Criminality/loader.lua
+++ Criminality/loader.lua
@@ -1,1 +1,20 @@
 Then, in your main script, load Orion UI locally:
+local Window = OrionLib:MakeWindow({
+    Name = "UniScript BETA",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "UniScript",
+    ConfigName = "Settings"
+})
+local Tab = Window:MakeTab({
+    Name = "Main",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+Tab:AddButton({
+    Name = "Test Button",
+    Callback = function()
+        print("Button clicked!")
+    end
+})
